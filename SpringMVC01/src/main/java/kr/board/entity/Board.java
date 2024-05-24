4274a069e070f10215369950ca09e1d0d000ab16
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2024-05-25 01:29:19 +0900

    9chap_wanle

--- SpringMVC01/src/main/java/kr/board/entity/Board.java
+++ SpringMVC01/src/main/java/kr/board/entity/Board.java
@@ -8,5 +8,44 @@ public class Board {
 	private String writer; //작성자 
 	private String indate; //작성일
 	private int count; //조회수
+	
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getIndate() {
+		return indate;
+	}
+	public void setIndate(String indate) {
+		this.indate = indate;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
 
 }
